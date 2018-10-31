9cae5575ab3c84e7cda173e3950e403c37bd1d87
Louise Hermosa <jayzces@users.noreply.github.com>
2018-10-31 16:45:32 +0800

    Script to open and close popup

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 const cards = document.querySelectorAll('.card')
 const button = document.querySelector('button')
 const mainContainer = document.querySelector('.page')
+const popupContainer = document.querySelector('.popup')
 
 const generateFocus = (parentClass, targetClass) => {
     if (targetClass) {
@@ -9,12 +10,14 @@ const generateFocus = (parentClass, targetClass) => {
         if (currentFocused != null) {
             currentFocused.classList.remove('focus')
             mainContainer.classList.remove('focusing')
+            popupContainer.classList.remove('open')
         }
 
         let children = document.querySelectorAll(`.${parentClass} .${targetClass}`)
         let focusIndex = Math.floor(Math.random() * children.length)
         children[focusIndex].classList.add('focus')
         mainContainer.classList.add('focusing')
+        popupContainer.classList.add('open')
     }
 }
 
@@ -35,6 +38,7 @@ cards.forEach(card => card.addEventListener('click', e => {
     if (card.classList.contains('focus')) {
         card.classList.remove('focus')
         mainContainer.classList.remove('focusing')
+        popupContainer.classList.remove('open')
     }
 
     if (e.target === e.currentTarget) card.classList.toggle('click')
